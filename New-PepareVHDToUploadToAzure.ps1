d7dfdeffab2e6061e4526f28eefbca59411bbc4b
Mike Dzikowski <micdz@microsoft.com>
2023-07-10 10:40:59 -0400

    encoding

--- New-PepareVHDToUploadToAzure.ps1
+++ New-PepareVHDToUploadToAzure.ps1
@@ -1,4 +1,4 @@
-# Global variables
+﻿# Global variables
 $Uri = "https://imagescripts.blob.core.usgovcloudapi.net/scripts/WindowsAzureVmAgent.2.7.41491.1044_2201181044.fre.msi" 
 $outfile = "$env:windir\temp\WindowsAzureVmAgent.2.7.41491.1044_2201181044.fre.msi"
 $loc = "$env:windir\temp\sanpolicy.txt"
